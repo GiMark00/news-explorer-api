6a891fee2bbb8de142e5f41ed18c5423633451d7
AHTOH <anton-peresvet@mail.ru>
2021-01-27 15:50:22 +0300

    v.0.0.8

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const AllRoutes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const whitelist = [
+  'http://localhost:8080',
+  'https://api.goosenews.students.nomoreparties.space',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  allowHeaders: 'Content-Type',
+  optionsSuccessStatus: 200,
+};
 
 const { PORT = 3000, DATA_URL, NODE_ENV } = process.env;
 
@@ -16,6 +35,7 @@ mongoose.connect(NODE_ENV === 'production' ? DATA_URL : 'mongodb://localhost:270
   useFindAndModify: false,
 });
 
+app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(requestLogger);
